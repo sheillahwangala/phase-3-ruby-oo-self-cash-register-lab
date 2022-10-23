a9fe18116fb30f6a4209324e589b258b2dea1215
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-10-23 20:54:34 +0300

    completed assignment

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,38 @@
+class CashRegister
+    attr_accessor :total, :items, :discount, :last_transaction
 
+    def initialize (discount=0)
+        @total=0
+        @discount = discount
+        @items = []
+        
+    end
+
+    def add_item(title, price, quantity=1)
+        self.last_transaction = price * quantity
+        self.total += price * quantity
+        quantity.times do
+        self.items << title
+        end
+
+      end
+
+      def apply_discount
+        if discount > 0
+            present_discount =  self.total * @discount/100
+            self.total = self.total-present_discount
+            "After the discount, the total comes to $#{self.total}."
+
+        else
+            "There is no discount to apply."
+
+        end
+    end
+
+    def void_last_transaction
+        self.total -= last_transaction
+    end
+
+
+      
+end
